2e9e4b80d482b94a45e8a96f0383ba30a03c3677
Denisse23 <denissesuazo@hotmail.com>
2016-05-14 21:01:11 -0600

    Guarda en el espacio de root, aparta cluster en la fattable por cada directorio, almacena la estrucutra en memoria

--- main.cpp
+++ main.cpp
@@ -4,106 +4,115 @@
 #include <stdio.h>
 #include <string.h>
 #include <vector>
+#include <stdlib.h>
 
 using namespace std;
-
-
-unsigned short int fatTable[65536];
-char cluster[4096];
-
-void crearDirectorioRoot(char, char*, int);
-void CrearArchivoRoot(char, char*, char*);
-void crearParticionFat();
-void ls();
 
 struct Directory{
 	char primer_caracter;
 	char nombre_archivo[10]; 
 	unsigned char atributos;
 	char fecha[8];
-	unsigned short int direccion;
+	unsigned short direccion;
 	unsigned int tamano;
 	char reservado[6];
 };
 
+struct Cluster{
+	char espacio[4096]; //Pueden ser 128 directorios o un archivo
+};
+
+unsigned short fatTable[65536];
+Directory root[512];
+Cluster Data[65468];
+
+void crearDirectorioRoot(char, char*, int);
+void CrearArchivoRoot(char, char*, char*);
+void crearParticionFat();
+void ls();
+void escribirFatFile();
+void getInfoFile();
+
 
 
 int main(){
-	cout<<"1) Crear archivo particion"<<endl;
-	cout<<"2) Agregar directorio a particion ya creada"<<endl;
-	cout<<"3) Agregar archivo a particion ya creado(archivo debe estar en el mismo directorio)"<<endl;
-	cout<<"4) ls"<<endl;
-	int opcion;
-	cout<<"Opcion: "; 
-	cin>>opcion;
-	switch(opcion){
-		case 1:
-			crearParticionFat();
+	getInfoFile(); // copia la informacion del archivo binario en las diferentes estructuras.
+	while(true){
+		cout<<"1) Formatear particion"<<endl;
+		cout<<"2) Agregar directorio a particion ya creada"<<endl;
+		cout<<"3) Agregar archivo a particion ya creado(archivo debe estar en el mismo directorio)"<<endl;
+		cout<<"4) ls"<<endl;
+		cout<<"5) Exit"<<endl;
+		int opcion;
+		cout<<"Opcion: "; 
+		cin>>opcion;
+		if(opcion==5){
+			escribirFatFile();
 			break;
-		case 2:	
-			char letra;	
-			cout<<"primera letra: ";
-			cin>>letra;
-			char nombre[10];	
-			cout<<"nombre_archivo: ";
-			cin>>nombre;
-			crearDirectorioRoot(letra,nombre,0);
-			break;
-		case 3:
-			cout<<"primera letra: ";
-			cin>>letra;	
-			cout<<"nombre_archivo: ";
-			cin>>nombre;
-			char archivo[10];
-			cout<<"nombre archivo a copiar: ";
-			cin>> archivo;
-			CrearArchivoRoot(letra, nombre, archivo);
-			break;
-		case 4:
-			ls();
-			break;
-		default:
-			break;
+		}
+		switch(opcion){
+			case 1:
+				crearParticionFat();
+				break;
+			case 2:	
+				char letra;	
+				cout<<"primera letra: ";
+				cin>>letra;
+				char nombre[10];	
+				cout<<"nombre_archivo: ";
+				cin>>nombre;
+				crearDirectorioRoot(letra,nombre,0);
+				break;
+			case 3:
+				cout<<"primera letra: ";
+				cin>>letra;	
+				cout<<"nombre_archivo: ";
+				cin>>nombre;
+				char archivo[10];
+				cout<<"nombre archivo a copiar: ";
+				cin>> archivo;
+				CrearArchivoRoot(letra, nombre, archivo);
+				break;
+			case 4:
+				ls();
+				break;
+			default:
+				break;
+		}
 	}
 	return 0;
 }
 
 void ls(){
-	char line[32];
-	char nombre[11];
-	ifstream fentrada;
-	fentrada.open("particionfat32",ios::binary|ios::out|ios::in);
-			
-	for(int i = 0; i<65536; i++){
-		 fentrada.read(line, 32);
-		 if(line[0]!=0){
-		 	memcpy( nombre, line + 1, 10 );
-		 	cout<<"Name: "<<nombre;
+	
+	for(int rot =0; rot<512; rot++){
+		 if(root[rot].primer_caracter!=0){
+		 	cout<<"Name: "<<root[rot].nombre_archivo;
 		 	cout<<" ";
-		 	cout<<"Tipo: "<<line[11]<<endl;
+		 	cout<<"Tipo: "<<root[rot].atributos<<endl;
 		 }
 	}
-	fentrada.close();
 }
 
 void crearParticionFat(){	
 	for(int i = 0; i<65536; i++)
 		fatTable[i] = 0;
     ofstream outfile;
-  	outfile.open("particionfat32", ios::binary | ios::out);
+  	outfile.open("particionfat16", ios::binary | ios::out);
   	outfile.write((char*)&fatTable, sizeof(short int)*65536);
   	outfile.write((char*)&fatTable, sizeof(short int)*65536);
-  	for(int i = 0; i<4096; i++){
-  		cluster[i] = NULL;
+  	for(int i = 0; i<512; i++){
+  		outfile.write((char*)&root, 32);
   	}
-  	for(int i = 0; i<65472; i++){
-  		outfile.write((char*)&cluster, 4096);
+  	/*
+  	for(int i = 0; i<65468; i++){
+  		outfile.write((char*)&Data[i].espacio, 4096);
   	}
+  	*/
   	outfile.close();
 }
 
 void crearDirectorioRoot(char primera_letra, char *nombre ,int tamano){
-	short int offset = 262144;
 	char tipo = 'd';
 	time_t now = time(0);
 	char* dt = ctime(&now);
@@ -111,43 +120,36 @@ void crearDirectorioRoot(char primera_letra, char *nombre ,int tamano){
 	memcpy ( fecha, dt, 8);
 	char nombre_archivo[10];
 	memcpy (nombre_archivo,nombre,10);
-	short int direccion = offset;
-	ifstream infile;
-	infile.open("particionfat32", ios::binary | ios::in);
-	infile.seekg(offset);
 	char reservado[6];
-	while(direccion<278528){
-		char temp[32];
-		infile.read (temp, 32);
-		if(temp[0] != NULL){
+	for(int rot =0; rot<512; rot++){ //ahora revisa en la region root
+		if(root[rot].primer_caracter != 0){
 			cout<<"hay algo"<<endl;
-			direccion += 32;
 		}else{
 			cout<<"vacio"<<endl;
-			Directory directorio ;
-			directorio.primer_caracter = primera_letra;
-			memcpy (directorio.nombre_archivo,nombre,10);
+			root[rot].primer_caracter = primera_letra;
+			memcpy (root[rot].nombre_archivo,nombre,10);
 			//directorio.nombre_archivo = nombre_archivo;
-			directorio.atributos = tipo;
-			memcpy (directorio.fecha, dt, 8);
+			root[rot].atributos = tipo;
+			memcpy (root[rot].fecha, dt, 8);
 			//directorio.fecha = fecha;
-			directorio.direccion = offset;
-			directorio.tamano = tamano;
-			ofstream outfile;
-			infile.close();
-			outfile.open("particionfat32",ios::binary|ios::out|ios::in);
-			outfile.seekp(direccion);
-			outfile.write((char*)&directorio,32);
-			outfile.close();
+			short int cluesterdireccion; // buscar cluster disponible en la fattable
+			for(int clus=69;clus<65536;clus++){
+				cout<<fatTable[clus]<<endl;
+				if(fatTable[clus]==0 ){
+					cluesterdireccion =clus;
+					fatTable[clus]= 63; // el cluster 63 es parte de la fattable(indica entonces que no apunta a otro cluster)
+					break;
+				}
+			}
+			root[rot].direccion = cluesterdireccion;
+			root[rot].tamano = tamano;
 			break;
 		}
 	}
-	if(direccion>278528)
-		infile.close();
 }
 
 void CrearArchivoRoot(char primera_letra, char *nombre ,char* nombre_archivo_a_copiar){
-	short int offset = 262144;
+	int offset = 131073;
 	char tipo = 'a';
 	time_t now = time(0);
 	char* dt = ctime(&now);
@@ -155,12 +157,12 @@ void CrearArchivoRoot(char primera_letra, char *nombre ,char* nombre_archivo_a_c
 	memcpy ( fecha, dt, 8);
 	char nombre_archivo[10];
 	memcpy (nombre_archivo,nombre,10);
-	short int direccion = offset;
+	int direccion = offset;
 	ifstream infile;
 	infile.open("particionfat32", ios::binary | ios::in);
 	infile.seekg(offset);
 	char reservado[6];
-	while(direccion<278528){
+	while(direccion<139264){
 		char temp[32];
 		infile.read (temp, 32);
 		if(temp[0] != NULL){
@@ -204,7 +206,7 @@ void CrearArchivoRoot(char primera_letra, char *nombre ,char* nombre_archivo_a_c
 			cout<<"pasa primer while"<<endl;
 			
 			ofstream outfile;
-			outfile.open("particionfat32",ios::binary|ios::out|ios::in);
+			outfile.open("particionfat16",ios::binary|ios::out|ios::in);
 
 			for(int i = 0;i<lista_enlazada.size()-1; i++){
 				outfile.seekp(lista_enlazada[i], ios::beg);
@@ -241,3 +243,53 @@ void CrearArchivoRoot(char primera_letra, char *nombre ,char* nombre_archivo_a_c
 	if(direccion>278528)
 		infile.close();
 }
+
+void getInfoFile(){
+
+
+	ifstream infile;
+	infile.open("particionfat16", ios::binary | ios::in);
+	infile.seekg(131072);
+	for(int i = 0; i<65536; i++){
+		char temp[1];
+		infile.read (temp, 1);
+		char temp2[1];
+		infile.read (temp2, 1);
+			fatTable[i]= (unsigned short)(temp[0]+temp2[0]);
+
+	}
+  	
+  	for(int i = 0; i<512; i++){
+  		infile.read((char*)&root[i], 32);
+  	}
+	
+	/*
+  	for(int i = 0; i<65468; i++){
+  		infile.read((char*)&Data[i], 4096);
+  	}
+  	*/
+  	infile.close();
+
+}
+
+void escribirFatFile(){
+	ofstream outfile;
+  	outfile.open("particionfat16", ios::binary | ios::out);
+  	outfile.write((char*)&fatTable, sizeof(short int)*65536);
+  	outfile.write((char*)&fatTable, sizeof(short int)*65536);
+  	
+  	for(int i = 0; i<512; i++){
+  		outfile.write((char*)&root[i], 32);
+  	}
+  	
+  		/*
+  	for(int i = 0; i<65468; i++){
+  		outfile.write((char*)&Data[i], 4096);
+  	}
+  	*/
+  	
+  	outfile.close();
+  	
+
+}
+
